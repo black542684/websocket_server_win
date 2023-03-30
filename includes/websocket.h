e138392a6d857c7c1e174ac433e70ab8d131df68
zhutongbin <zhutb@or-intech.com>
2023-03-30 15:27:24 +0800

    客户端消息的接收和解码

--- includes/websocket.h
+++ includes/websocket.h
@@ -4,10 +4,25 @@
 
 #ifndef WEBSOCKET_SERVER_WIN_WEBSOCKET_H
 #define WEBSOCKET_SERVER_WIN_WEBSOCKET_H
+
 #include <WinSock2.h>
 #include <process.h>
 // 处理客户端请求的线程函数
 unsigned WINAPI accept_request(void* arg);
+
 // 和客户端完成握手之后，接收和处理客户端的消息
 void handleMessage(SOCKET client);
+
+// 获取接收消息的类型
+void parintMessageType(char type);
+
+// 获取消息长度
+long long getMessageLength(unsigned char len, SOCKET client);
+
+// 获取解码消息的掩码
+void getMask(char* mask, SOCKET client);
+
+// 获取消息体长度并且解码
+void encodeMessageBody(const char* mask, long long len, SOCKET client);
+
 #endif //WEBSOCKET_SERVER_WIN_WEBSOCKET_H
--- src/websocket.cpp
+++ src/websocket.cpp
@@ -8,6 +8,8 @@
 #include "../lib/sha1.hpp"
 #include <map>
 
+using namespace std;
+
 
 unsigned WINAPI accept_request(void* arg) {
     auto client = (SOCKET)arg; // 客户端套接字
@@ -52,7 +54,7 @@ unsigned WINAPI accept_request(void* arg) {
      * 然后用SHA1编码，再用base64编码一次
      */
 
-    std::cout << "Sec-WebSocket-Key: "<< request_head["Sec-WebSocket-Key"] << std::endl;
+    // std::cout << "Sec-WebSocket-Key: "<< request_head["Sec-WebSocket-Key"] << std::endl;
 
     char hex[SHA1_HEX_SIZE];
     char base64[SHA1_BASE64_SIZE];
@@ -68,7 +70,7 @@ unsigned WINAPI accept_request(void* arg) {
     .print_hex(hex)
     .print_base64(base64);
 
-    std::cout << "Sec-WebSocket-Accept: "<< base64 << std::endl;
+    // std::cout << "Sec-WebSocket-Accept: "<< base64 << std::endl;
 
     // 当服务器收到握手请求时，它应该发回一个特殊的响应，表明协议将从 HTTP 变为 WebSocket
     buff = "HTTP/1.1 101 Switching Protocols\r\n";
@@ -86,8 +88,11 @@ unsigned WINAPI accept_request(void* arg) {
     buff = "\r\n";
     send(client, buff.c_str(), buff.length(), 0);
 
-    printf("握手结束\n");
-    handleMessage(client);
+    printf("握手结束，开始正式通讯！\n");
+
+    while (true) {
+        handleMessage(client);
+    }
 
 
     std::cout << "结束" << std::endl;
@@ -96,17 +101,150 @@ unsigned WINAPI accept_request(void* arg) {
 
 
 void handleMessage(SOCKET client) {
-    printf("开始接收消息...\n");
+    // printf("开始接收消息...\n");
     char c = '\0'; // 保存读取到的一个字符
-    while (true) {
-        int n = recv(client, &c, 1, 0);
-        if (n > 0) {
-            char s[10];
-            itoa(c, s, 2);
-            s[8] = '\0';
-            printf("%s ", s);
-        } else {
+
+    recv(client, &c, 1, 0); // 读取第一个字节
+    // 消息类型
+    char opcode = c & (char)0x0F;
+    parintMessageType(opcode);
+
+    // 获取消息长度
+    recv(client, &c, 1, 0);
+    long long length = getMessageLength(c, client);
+
+    // 获取解码消息的掩码字节-4个字节
+    char mask[4] = {0};
+    getMask(mask, client);
+
+    // 获取消息体
+    encodeMessageBody(mask, length, client);
+}
+
+void parintMessageType(char type) {
+    switch (type) {
+        case 0x0:
+            cout << "延续帧" << endl;
             break;
-        }
+        case 0x01:
+            cout << "文本帧" << endl;
+            break;
+        case 0x02:
+            cout << "二进制帧" << endl;
+            break;
+        case 0x03:
+        case 0x04:
+        case 0x05:
+        case 0x06:
+        case 0x07:
+            cout << "保留给更多的非控制帧" << endl;
+            break;
+        case 0x08:
+            cout << "连接关闭" << endl;
+            break;
+        case 0x09:
+            cout << "ping" << endl;
+            break;
+        case 0x0A:
+            cout << "pong" << endl;
+            break;
+        case 0x0B:
+        case 0x0C:
+        case 0x0D:
+        case 0x0E:
+        case 0x0F:
+            cout << "为进一步的控制帧保留" << endl;
+            break;
+
     }
+}
+
+long long getMessageLength(unsigned char len,  SOCKET client) {
+    /**
+     * 第二个字节
+     * 第 1 位定义了是否"Payload data"进行了掩码计算，
+     * 如果值设置为 1，那么在 Masking-Key 字段中会有一个掩码密钥，
+     * 并且它可以用来进行"Payload data"的去掩码计算
+     *
+     * 剩下的 2 - 8 位组成一个二进制数：
+     * 如果这个值在 0 与 125 之间，那么这个值就是消息的长度。
+     * 如果这个值是 126，那么接下来的 2 个字节（16 位无符号整数）是消息长度。
+     * 如果这个值是 127，那么接下来的 8 个字节（64 位无符号整数）是消息长度。
+     */
+    len = len & 127;
+    long long length = 0;
+
+    char c = '\0'; // 保存读取到的一个字符
+    if (len <= 125) {
+        length = len;
+    } else if (len == 126) {
+        recv(client, &c, 1, 0);
+        length += c;
+        length << 8;
+
+        recv(client, &c, 1, 0);
+        length += c;
+    } else if (len == 127) {
+        recv(client, &c, 1, 0);
+        length += c;
+        length << 8;
+
+        recv(client, &c, 1, 0);
+        length += c;
+        length << 8;
+
+        recv(client, &c, 1, 0);
+        length += c;
+        length << 8;
+
+        recv(client, &c, 1, 0);
+        length += c;
+        length << 8;
+
+        recv(client, &c, 1, 0);
+        length += c;
+        length << 8;
+
+        recv(client, &c, 1, 0);
+        length += c;
+        length << 8;
+
+        recv(client, &c, 1, 0);
+        length += c;
+        length << 8;
+
+        recv(client, &c, 1, 0);
+        length += c;
+    }
+
+
+    ::printf("消息长度: %lld \n", length);
+    return length;
+}
+
+void getMask(char* mask, SOCKET client) {
+    char c = '\0';
+    for (int i = 0; i < 4; i++) {
+        recv(client, &c, 1, 0);
+        mask[i] = c;
+    }
+}
+
+void encodeMessageBody(const char* mask, long long len, SOCKET client) {
+    char* encoded = new char[len]; // 存放未解码的消息
+    std::string decoded; // 存放解码后的消息
+    char c = '\0';
+
+    // 获取消息体
+    for (int i = 0; i < len; i++) {
+        recv(client, &c, 1, 0);
+        encoded[i] = c;
+    }
+
+    // 对消息进行解码
+    for (int i = 0; i < len; i++) {
+        decoded.push_back(encoded[i] ^ mask[i % 4]);
+    }
+
+    cout << "客户端: " << decoded << endl;
 }
